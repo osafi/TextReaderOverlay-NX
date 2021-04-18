627ca4f10d2bc9480c13ddf5dc22e5fa13a0b0d8
Omeed Safi <omeed@safi.ms>
2021-04-18 13:46:13 -0700

    Fix clipping on LogMenu

--- source/LogMenu.cpp
+++ source/LogMenu.cpp
@@ -6,8 +6,8 @@ tsl::elm::Element* LogMenu::createUI() {
     auto *frame = new tsl::elm::OverlayFrame("Log Menu", "");
 
     frame->setContent(new tsl::elm::CustomDrawer([](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h) {
-        Log::forEach([renderer](std::string const &msg, int i) {
-            renderer->drawString(msg.c_str(), false, 20, 80 + i*12, 12, a(0xFFFF));
+        Log::forEach([renderer, x, y](std::string const &msg, int i) {
+            renderer->drawString(msg.c_str(), false, x, y + 12 + i*12, 12, a(0xFFFF));
         });
     }));
 
