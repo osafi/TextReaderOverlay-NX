36a451bea7294222ce6e7f99a943139b5fd968f4
Omeed Safi <omeed@safi.ms>
2021-04-18 10:47:41 -0700

    Quick fix for browsing sdmc

--- source/main.cpp
+++ source/main.cpp
@@ -4,5 +4,8 @@
 #include <Overlay.hpp>
 
 int main(int argc, char **argv) {
+    fsdevMountSdmc();
+    tsl::hlp::ScopeGuard dirGuard([]{ fsdevUnmountDevice("sdmc"); });
+
     return tsl::loop<TextReaderOverlay>(argc, argv);
 }
